065215bb8ebf197ac91ce131910409efd4a7fab8
Taylor Kerr <tckerr14@gmail.com>
2019-10-23 13:17:27 -0400

    environment variable for local testing

--- src/dynamo.js
+++ src/dynamo.js
@@ -1,9 +1,18 @@
 const AWS = require('aws-sdk');
-const dynamo = new AWS.DynamoDB.DocumentClient(/*{
-    region: 'localhost', // localhost for local dev
-    endpoint: 'http://localhost:8000',  // http://localhost:8000 for local dev
-    accessKeyId: 'DEFAULT_ACCESS_KEY',  // needed if you don't have aws credentials at all in env
-    secretAccessKey: 'DEFAULT_SECRET' // needed if you don't have aws credentials at all in env
-}*/)
+const profile = process.env.AWS_PROFILE;
+
+let options = {}
+
+// sets the local variables to test locally instead of using the real resources
+if (profile === 'offline') {
+	options = {
+    region: 'localhost',
+    endpoint: 'http://localhost:8000', 
+    accessKeyId: 'DEFAULT_ACCESS_KEY', 
+    secretAccessKey: 'DEFAULT_SECRET'
+	}
+}
+
+const dynamo = new AWS.DynamoDB.DocumentClient(options)
 
 module.exports = dynamo;
