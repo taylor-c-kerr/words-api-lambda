451b8cfa47b2a79557341220708c53efffa76351
Taylor Kerr <tckerr14@gmail.com>
2020-05-14 17:56:05 -0400

    adds list query param

--- src/handlers/src/list.js
+++ src/handlers/src/list.js
@@ -22,16 +22,20 @@ const list = async (event) => {
 		response.body = JSON.stringify(dynamoResponse);
 	
 		if (queryStringParameters) {
-			// look up the name and return;
-			const {name} = queryStringParameters;
+			const {name, list} = queryStringParameters;
 			if (name) {
 				const exists = dynamoResponse.filter(entry => entry.name === name);
 				response.body = JSON.stringify(exists);
 				response.statusCode = !exists.length ? 404 : exists.length ? 200 : 500;
+				return response
+			}
+			else if (list) {
+				const filtered = dynamoResponse.filter(entry => entry.category.includes(list));
+				response.body = JSON.stringify(filtered);
+				response.statusCode = !filtered.length ? 404 : filtered.length ? 200 : 500;
+				return response
 			}
 		};
-	
-		return response;	
 	}
 	catch(error) {
 		return {
