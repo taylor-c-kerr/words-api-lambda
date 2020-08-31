5ac3564aeb99913f1cf8edbbeac2dd1dfcd70b53
Taylor Kerr <tckerr14@gmail.com>
2020-08-30 20:25:23 -0400

    first pass at list model route

--- src/handlers/model-route/model-list.js
+++ src/handlers/model-route/model-list.js
@@ -2,13 +2,11 @@ const createResponse = require('../../services/response');
 const Request = require('../../controllers/Request');
 const request = new Request();
 
-class ModelListRoute {
+module.exports = class ModelListRoute {
   constructor(event, stages) {
-    this.body = JSON.parse(event.body);
-    this.format = stages.format;
-    this.validate = stages.validate;
-    this.checkDupes = stages.checkDupes;
-    this.defineResponse = stages.defineResponse;
+    this.queryParams = event.queryStringParameters;
+    this.filter = stages.filter;
+    this.results = null;
   }
 
   handleError(error) {
@@ -17,7 +15,16 @@ class ModelListRoute {
 
   async list() {
     try {
-      
+      this.results = await request.list();
+
+      // handle queryParams
+      if (this.filter && this.queryParams) {
+        this.results = this.filter(this.queryParams, this.results);
+      }
+
+      // don't know why I am filtering by namd in the words list handler
+      // return createResponse(200, dynamoResponse.filter(res => res.name));
+      return createResponse(200, this.results);
     } catch (error) {
       return createResponse(500, { error: error.message });
     }
--- src/handlers/words/src/list.js
+++ src/handlers/words/src/list.js
@@ -1,16 +1,35 @@
 const createResponse = require('../../../services/response');
 const Request = require('../../../controllers/Request');
 const request = new Request();
+const ModelListRoute = require('../../model-route/model-list');
 
-/**
- * @returns {object} response The data, including all the data from the db, to be used in the server's response
-*/
-const list = async (event) => {
+const filterName = (results, name) => {
+	return results.filter(result => result.name === name)
+};
+const filterList = (results, listValue) => {
+	return results.filter(result => result && result.category && result.category.includes(listValue))
+};
+
+const filter = (param, results) => {
+	if (!param) return results;
+	const filterParams = ['name', 'list'];
+	if (!filterParams.includes(param)) {
+		throw Error('Invalid query param');
+	}
+	if (param === 'name') {
+		return filterName(results, param);
+	} else if (param === 'list') {
+		return filterList(results, param);
+	}
+}
+
+const listOld = async (event) => {
 	try {
 		const {queryStringParameters} = event;
 		const dynamoResponse = await request.list();
 	
-		if (queryStringParameters) {
+		/* if (queryStringParameters) {
+			filter(queryStringParameters, dynamoResponse)
 			const {name, list} = queryStringParameters;
 			if (name) {
 				const byName = dynamoResponse.filter(entry => entry.name === name);
@@ -21,19 +40,28 @@ const list = async (event) => {
 				return createResponse(500, {error: 'error filtering by name'});
 			}
 			else if (list) {
-				const byList = dynamoResponse.filter(entry => entry.category.includes(list));
+				const byList = dynamoResponse.filter(entry => {
+					return entry && entry.category && entry.category.includes(list)
+				});
 				if (byList) {
 					const statusCode = byList.length ? 200 : 404;
 					return createResponse(statusCode, byList)
 				}
 				return createResponse(500, {error: 'error filtering by list'});
 			}
-		};
+		}; */
 		return createResponse(200, dynamoResponse.filter(res => res.name));
 	}
 	catch(error) {
 		return createResponse(500, {error: error.message});
 	}
+}
+
+const list = async (event) => {
+	const listHandler = new ModelListRoute(event, {
+		filter
+	});
+	return await listHandler.list();
 }
 
 exports.handler = list;
