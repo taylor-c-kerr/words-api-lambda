80b31483b852fa5e52e8016cf304a6425a0d16e2
Taylor Kerr <tckerr14@gmail.com>
2020-09-10 23:00:14 -0400

    first pass at using model update route

--- src/handlers/model-route/model-update.js
+++ src/handlers/model-route/model-update.js
@@ -2,22 +2,51 @@ const createResponse = require('../../services/response');
 const Request = require('../../controllers/Request');
 const request = new Request();
 
-class ModelUpdateRoute {
+module.exports = class ModelUpdateRoute {
   constructor(event, stages) {
     this.body = JSON.parse(event.body);
+    this.id = event.pathParameters.id;
     this.format = stages.format;
     this.validate = stages.validate;
-    this.checkDupes = stages.checkDupes;
+    this.checkForUnsupportedUpdate = stages.checkForUnsupportedUpdate;
     this.defineResponse = stages.defineResponse;
   }
 
   handleError(error) {
+    if (error === 404) return createResponse(404);
     return createResponse(400, { error });
   }
 
   async update() {
     try {
-      
+      /* format data stage */
+      if (this.format) {
+        const { error } = this.format(this.body);
+        if (error) {
+          return this.handleError(error);
+        }
+      }
+
+      /* validate data stage */
+      if (this.validate) {
+        const result = this.validate(this.body);
+        if (result && result.error) {
+          return this.handleError(result.error);
+        }
+      }
+
+      /* check for bad update stage */
+      const error = await this.checkForUnsupportedUpdate(this.body);
+      if (error) {
+        return this.handleError(error)
+      }
+
+      /* make request */
+		  await request.update(this.body);
+
+      /* format and return response stage */
+      const response = this.defineResponse(this.body);
+      return createResponse(201, response)
     } catch (error) {
       return createResponse(500, { error: error.message });
     }
--- src/handlers/words/src/update.js
+++ src/handlers/words/src/update.js
@@ -1,57 +1,103 @@
 const Validate = require('../../../services/validation');
-const createResponse = require('../../../services/response');
-const format = require('../../../services/formatter');
+// const createResponse = require('../../../services/response');
+const formatter = require('../../../services/formatter');
 const Request = require('../../../controllers/Request');
 const request = new Request();
 const _ = require('lodash');
+const ModelUpdateRoute = require('../../model-route/model-update');
 
-/**
- * @param {string} id The id of the word to be updated
- * @param {object} word The data to be updated
- * @returns {object} response The data to be used in the server's response
-*/
-const getIsNameChanged = async (body) => {
-	const { name, id } = body;
-	const allWordsByName = await request.list({name});
-	const wordById = await request.get(id);
-	return allWordsByName.length === 0 || wordById.Item.name !== name;
+// /**
+//  * @param {string} id The id of the word to be updated
+//  * @param {object} word The data to be updated
+//  * @returns {object} response The data to be used in the server's response
+// */
+// const getIsNameChanged = async (body) => {
+// 	const { name, id } = body;
+// 	const allWordsByName = await request.list({name});
+// 	const wordById = await request.get(id);
+// 	return allWordsByName.length === 0 || wordById.Item.name !== name;
+// }
+
+const getWordById = (id) => request.get(id);
+
+const getWordsByName = (name) => request.list({name});
+
+const handleId = (word, id) => {
+	if (!word.id) {
+		word.id = id;
+	} else if (word.id !== id) {
+		return 'Incorrect ID';
+	}
+};
+
+
+const checkForUnsupportedUpdate = async (word, id) => {
+	const isIdIncorrect = handleId(word, id);
+	if (isIdIncorrect) return isIdIncorrect;
+	const [wordById, wordsByName] = await Promise.all([getWordById(word.id), getWordsByName(word.name)]);
+	
+	if (_.isEmpty(wordById)) {
+		return 404;
+	}
+
+	if (wordyById.Item.name !== word.name || wordsByName.length === 0) {
+		return `Can't change name`;
+	}
 }
 
-const update = async (event) => {
-	try {
-		let { pathParameters, body } = event;
-		const { id } = pathParameters;
-		body = JSON.parse(body);
-		body = format(body);
+const validate = (word) => {
+	const result = Validate.updateWord(word);
+	if (result.error.length) return result.error;
+};
 
-		const validatedWord = Validate.updateWord(body);
-		let { valid, error, word, warning } = validatedWord;
-		if (!valid) {
-			return createResponse(400, { error });
-		}
+const format = (word) => formatter(word);
 
-		const idExists = await request.get(id);
-		if (_.isEmpty(idExists)) {
-			return createResponse(404);
-		}
+const defineResponse = (word) => { word };
 
-		if (!word.id) {
-			word.id = id;
-		} else if (word.id !== id) {
-			return createResponse(400, { error: 'incorrect id' });
-		}
+// const updateOld = async (event) => {
+	// try {
+		// let { pathParameters, body } = event;
+		// const { id } = pathParameters;
+		// body = JSON.parse(body);
+		// body = format(body);
+
+		// const validatedWord = Validate.updateWord(body);
+		// let { valid, error, word, warning } = validatedWord;
+		// if (!valid) {
+		// 	return createResponse(400, { error });
+		// }
+
+		// const idExists = await request.get(id);
+		// if (_.isEmpty(idExists)) {
+		// 	return createResponse(404);
+		// }
+
+		// if (!word.id) {
+		// 	word.id = id;
+		// } else if (word.id !== id) {
+		// 	return createResponse(400, { error: 'incorrect id' });
+		// }
 		
-		const isNameChanged = await getIsNameChanged(word);
-		if (isNameChanged) {
-			return createResponse(400, { error: `can't change name` });
-		}
+		// const isNameChanged = await getIsNameChanged(word);
+		// if (isNameChanged) {
+		// 	return createResponse(400, { error: `can't change name` });
+		// }
 
-		await request.update(word);
-		return createResponse(201, { word, warning });
-	}
-	catch(error) {
-		return createResponse(500, { error: error.message });
-	}
+	// 	await request.update(word);
+	// 	return createResponse(201, { word, warning });
+	// }
+	// catch(error) {
+	// 	return createResponse(500, { error: error.message });
+	// }
+// }
+
+const update = async (event) => {
+	const UpdateHandler = new ModelUpdateRoute(event, {
+		format,
+		validate,
+		checkForUnsupportedUpdate,
+		defineResponse
+	})
 }
 
 exports.handler = update;
