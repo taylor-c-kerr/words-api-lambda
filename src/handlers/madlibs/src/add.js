0d72b838a0e768b2085cc8bcb6d1b311ab59fb62
Taylor Kerr <tckerr14@gmail.com>
2020-09-13 20:43:34 -0400

    uses model route

--- src/handlers/madlibs/src/add.js
+++ src/handlers/madlibs/src/add.js
@@ -1,59 +1,31 @@
-const createResponse = require('../../../services/response');
 const Validate = require('../../../services/validation');
-// const format = require('../../../services/formatter');
 const Request = require('../../../controllers/Request');
 const request = new Request();
 const _ = require('lodash');
+const ModelAddRoute = require('../../model-route/model-add');
 
-/**
- * @param {object} madlib
- * @returns {string}
-*/
-const getIsAlreadyAdded = async (madlib) => {
+const checkDupes = async (madlib) => {
 	const { id, title } = madlib;
-	let message;
-
 	const idExists = await request.get(id);
-	if (!_.isEmpty(idExists)) {
-		return 'ID already exists';
-	}
-
+	if (!_.isEmpty(idExists)) return 'ID already exists';
 	const allMadlibs = await request.list( { title });
 	const titleExists = allMadlibs.filter(madlib => madlib.title.toLowerCase() === title.toLowerCase());
-	message = titleExists.length > 0 ? 'Madlib already exists' : '';
-	return message;
+	return titleExists.length > 0 ? 'Madlib already exists' : '';
 }
 
-/**
- * @param {object} word The validated word to be added to the database
- * @returns {object} response The data to be used in the server's response
-*/
+const validate = (madlib) => {
+	const result = Validate.addMadlib(madlib);
+	if (result.error.length) return result.error;
+}
+
+const defineResponse = (madlib) => ({ madlib });
+
 const add = async (event) => {
-	try {
-		let { body } = event;
-		body = JSON.parse(body);
-	
-		const isAlreadyAdded = await getIsAlreadyAdded(body);
-		if (isAlreadyAdded) {
-			return createResponse(400, {error: 'item already exists'});
-		};
-
-		const validatedMadlib = Validate.addMadlib(body);
-		const { valid, error, warning, madlib } = validatedMadlib;
-		console.log(validatedMadlib)
-	
-		if (!valid) {
-			return createResponse(400, { error })
-		}
-		else {
-			await request.add(madlib);
-			return createResponse(201, { madlib, warning })
-		}
-	}
-	catch (error) {
-		return createResponse(500, {error: error.message})
-	}
-
+	const route = new ModelAddRoute(event, {
+		validate,
+		checkDupes,
+		defineResponse
+	});
+	return await route.add();
 }
-
 exports.handler = add;
